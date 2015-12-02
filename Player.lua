b21219ca691fb7b31f5854902f15f3717beb4b53
Cody <nil>
2015-12-02 10:54:33 -0500

    Commented and changed way constructor worked

--- Player.lua
+++ Player.lua
@@ -1,22 +1,30 @@
 
 local Player = {}
 
--- stats is a dictionary which holds Variable -> Value amendments
-Player.new = function(stats)
+Player.new = function(statics, stats)
+	-- tables are string keys (dictionaries)
+	-- Stats are not static, but introduced as an external table, should be integers
+	-- Data are not static
+	-- statics gets unpacked into this, prevents popping the metatable for no reason
 	local this = {
 		Stats = stats;
 		Data = {};
 	}
+	for i, v in pairs(statics)do
+		this[i] = v
+	end
 	
 	setmetatable(this, {
+		-- if not in this, find from stats, else data
 		__index = function(self, i)
-			return self.Data[i] or self.Stats[i]
+			return self.Stats[i] or self.Data[i]
 		end;
 		__newindex = function(self, i, v)
 			rawset(self, i, v)
 		end;
 	})
 	
+	-- get string keys of all your stats
 	this.GetStatsIndexes = function(self)
 		local out = {}
 		for i, v in pairs(self.Stats)do
@@ -25,6 +33,7 @@ Player.new = function(stats)
 		return out
 	end
 	
+	-- get string keys of all your data
 	this.GetDataIndexes = function(self)
 		local out = {}
 		for i, v in pairs(self.Data)do
@@ -33,6 +42,7 @@ Player.new = function(stats)
 		return out
 	end
 	
+	-- sums all your stats values
 	this.GetTotalStats = function(self)
 		local out = 0
 		for i, v in pairs(self.Stats)do
