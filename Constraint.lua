fbb4a3f14159a17356736cd2f56aa3e101b6ea90
Cody <nil>
2015-12-02 14:26:40 -0500

    Added comments

--- Constraint.lua
+++ Constraint.lua
@@ -1,6 +1,7 @@
 
 local Constraint = {}
 
+-- returns a number either the minimum or maximum specified value if the given number is less than or greater than the limits, else itself
 Constraint.constrain = function(m, v, ma)
 	return v < m and m or v > ma and ma or v
 end
@@ -12,6 +13,7 @@ Constraint.iLimit = function(m, va, ma)
 		Max = ma;
 	}
 	
+	-- basic calls to each operation
 	return setmetatable(this, {
 		__add = function(self, v)
 			self.Value = Constraint.constrain(self.Min, self.Value + v, self.Max)
