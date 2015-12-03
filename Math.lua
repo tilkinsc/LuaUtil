b80faf545cf34f500a6cdabadbdea6c84ff0a984
Cody <nil>
2015-12-03 13:11:40 -0500

    Added Math.lua, Table.lua, Vector2.lua... Vector2.lua depends on Math, Math depends on Table, Table is independant.

---
+++ Math.lua
@@ -0,0 +1,91 @@
+
+local Table = require("Table")
+
+local Math = {}
+
+Math.nlog =  2.718281828459
+
+-- low 'float' precision linear interpolation, 3 
+-- start is beginning number
+-- ending is goal number
+-- alpha is normalized distance
+Math.lerpl = function(start, ending, alpha)
+	return start + (ending - start) * alpha
+end
+
+-- high 'double' precision linear interpolation
+-- start is beginning number
+-- ending is goal number
+-- alpha is normalized distance
+Math.lerph = function(start, ending, alpha)
+	return (1 - alpha) * start + ending * alpha
+end
+
+-- the pythagorean theorem
+-- x is the x in the coordinate system
+-- y is the y in the coordinate system
+Math.pythag = function(x, y)
+	return math.sqrt(x * x + y * y)
+end
+
+-- adds all values of a tuple together, adding to the number specified
+-- start's value is reassigned then added to and finally returned
+-- the tuple is only numbers or canonical number strings
+Math.sum = function(start, ...)
+	local out = start
+	for _, v in pairs({...})do
+		out = out + v
+	end
+	return out
+end
+
+-- like Math.sum, but subtracts all the numbers instead of adding them
+Math.diff = function(start, ...)
+	local out = start
+	for _, v in pairs({...})do
+		out = out - v
+	end
+	return out
+end
+
+-- finds the mean of a tuple
+Math.mean = function(...)
+	local tab = {...}
+	return Math.sum(0, tab) / #tab
+end
+
+-- finds the mean of a tuple
+Math.median = function(...)
+	local tab = {...}
+	local register = table.sort(tab)
+	local middle = #tab / 2
+	if(#tab % 2 == 0)then
+		return (register[middle] + register[middle+1]) / 2
+	end
+	return register[middle]
+end
+
+-- finds the mode of a tuple
+Math.mode = function(...)
+	local register = Table.count({...})
+	local highest = register[1]
+	for i=2, #register do
+		highest = register[i] > highest and register[i] or highest
+	end
+	return highest
+end
+
+-- finds the range of a tuple
+Math.range = function(...)
+	local tab = {...}
+	return math.max(unpack(tab)) - math.min(unpack(tab))
+end
+
+-- returns the power of a number whose exponent must be negative
+-- number is the leading coefficient
+-- exponent is the negative number, which is the exponent
+Math.npow = function(number, exponent)
+	return 1 / math.pow(number, exponent)
+end
+
+return Math
---
+++ Vector2.lua
@@ -0,0 +1,79 @@
+
+local Math = require("Math")
+
+local Vector2 = {}
+
+Vector2.new = function(x, y, z)
+	local this = {
+		X = x or 0;
+		Y = y or 0;
+	}
+	this.magnitude = math.sqrt(x * x + y * y)
+	
+	-- low 'float' precision linear interpolation
+	-- v is ending vector2
+	-- a is normalized distance ratio between point a and b
+	this.lerpl = function(self, v, a)
+		return Vector2.new(
+			Math.lerpl(self.X, v.X, a),
+			Math.lerpl(self.Y, v.Y, a)
+		)
+	end
+	
+	-- high 'double' precision linear interpolation
+	-- v is goal number
+	-- a is normalized distance ratio between point a and b
+	this.lerph = function(self, v, a)
+		return Vector2.new(
+			Math.lerph(self.X, v.X, a),
+			Math.lerph(self.Y, v.Y, a)
+		)
+	end
+	
+	-- when indexing, unit will, if called, be added to the field space with a normalized unit vector of X and Y in the field space
+	-- just standard operations that manipulate X and Y based off the operator you are doing
+	return setmetatable(this, {
+		__index = function(self, i)
+			if(i == "unit")then
+				self.unit = Vector2.new(self.X / self.magnitude, self.Y / self.magnitude)
+				return self.unit
+			end
+		end;
+		__add = function(self, v)
+			local t = type(v) == "number"
+			self.X = self.X + (t and v or v.X)
+			self.Y = self.Y + (t and v or v.Y)
+			return self
+		end;
+		__sub = function(self, v)
+			local t = type(v) == "number"
+			self.X = self.X - (t and v or v.X)
+			self.Y = self.Y - (t and v or v.Y)
+			return self
+		end;
+		__mul = function(self, v)
+			local t = type(v) == "number"
+			self.X = self.X * (t and v or v.X)
+			self.Y = self.Y * (t and v or v.Y)
+			return self
+		end;
+		__div = function(self, v)
+			local t = type(v) == "number"
+			self.X = self.X / (t and v or v.X)
+			self.Y = self.Y / (t and v or v.Y)
+			return self
+		end;
+		__mod = function(self, v)
+			local t = type(v) == "number"
+			self.X = self.X % (t and v or v.X)
+			self.Y = self.Y % (t and v or v.Y)
+			return self
+		end;
+		__unm = function(self)
+			self.X = -self.X
+			self.Y = -self.Y
+		end;
+	})
+end
+
+return Vector2
