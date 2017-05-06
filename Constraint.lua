c03767e1086d77dd9b3e1c9278a69159782762a4
bud <bud@BudPc>
2017-05-06 18:33:37 -0400

    updated constraint.lua to be modern, added avoid feature

--- Constraint.lua
+++ Constraint.lua
@@ -1,20 +1,18 @@
 	
 local Constraint = {}
 
--- returns a number either the minimum or maximum specified value if the given number is less than or greater than the limits, else itself
+-- returns a number that, if is greater than ma, ma, or less than m, m, else v
 Constraint.constrain = function(m, v, ma)
 	return v < m and m or v > ma and ma or v
 end
 
-Constraint.iLimit = function(m, va, ma)
-	local this = {
-		Min = m;
-		Value = va;
-		Max = ma;
-	}
-	
-	-- basic calls to each operation
-	return setmetatable(this, {
+-- returns a number that, if is in range of m to ma, returns a default number, d, else v
+Constraint.avoid = function(m, v, ma, d)
+	return (v >= m and v <= ma) and d or v
+end
+
+-- basic evaluations to each operation
+local Constraint_metatable_constrain = {
 		__add = function(self, v)
 			self.Value = Constraint.constrain(self.Min, self.Value + v, self.Max)
 			return self
@@ -35,7 +33,44 @@ Constraint.iLimit = function(m, va, ma)
 			self.Value = Constraint.constrain(self.Min, self.Value % v, self.Max)
 			return self;
 		end;
-	})
+	}
+
+-- basic evaluations to each operation
+local Constraint_metatable_avoid = {
+		__add = function(self, v)
+			self.Value = Constraint.avoid(self.Min, self.Value + v, self.Max, self.Default)
+			return self
+		end;
+		__mul = function(self, v)
+			self.Value = Constraint.avoid(self.Min, self.Value * v, self.Max, self.Default)
+			return self
+		end;
+		__div = function(self, v)
+			self.Value = Constraint.avoid(self.Min, self.Value / v, self.Max, self.Default)
+			return self
+		end;
+		__sub = function(self, v)
+			self.Value = Constraint.avoid(self.Min, self.Value - v, self.Max, self.Default)
+			return self
+		end;
+		__mod = function(self, v)
+			self.Value = Constraint.avoid(self.Min, self.Value % v, self.Max, self.Default)
+			return self;
+		end;
+	}
+
+-- returns a new Constraint object
+-- if d == true, use the avoid metatable
+Constraint.iLimit = function(m, va, ma, d)
+	local this = {
+		Min = m;
+		Value = va;
+		Max = ma;
+		Default = d;
+	}
+	
+	if(d) return setmetatable(this, Constraint_metatable_avoid)
+	return setmetatable(this, Constraint_metatable_constrain)
 end
 
 return Constraint
